2c9e2734bb16995bc430061b7c685f84f62096c4
rani93 <114481571+rani93@users.noreply.github.com>
2023-06-10 10:17:12 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,27 @@
+
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var a=randomNumber1;
+
+var imagePick= "images/dice"+randomNumber1+".png";
+
+var b=document.querySelectorAll("img")[0];
+b.setAttribute("src", imagePick);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var d=randomNumber2;
+
+var imagePick2= "images/dice"+randomNumber2+".png";
+
+var c=document.querySelectorAll("img")[1];
+c.setAttribute("src", imagePick2);
+if(a==d){
+    document.querySelector("h1").innerHTML="draw!";
+}
+else if(a>d){
+    document.querySelector("h1").innerHTML="🚩Player1 wins";
+    
+}
+else{
+    document.querySelector("h1").innerHTML="player2 wins🚩";
+    
+}
